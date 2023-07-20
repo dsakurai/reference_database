f43b1ff843bd6127305e481302ccfe8a0f191b24
Daisuke Sakurai <d.sakurai0@gmail.com>
2023-07-20 17:27:20 +0900

    Add PDF thumbnail

--- DatabaseUI.py
+++ DatabaseUI.py
@@ -1,9 +1,10 @@
 import sys
 from PyQt6.QtWebEngineWidgets import QWebEngineView
-from PyQt6 import QtCore, QtWidgets
+from PyQt6 import QtCore, QtWidgets, QtGui
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QFrame, QPushButton, QMainWindow
 from PyQt6.QtWidgets import QSizePolicy
 from PyQt6.QtCore import QMimeDatabase, QMimeType, QSize
+from PyQt6.QtPdf import QPdfDocument
 from pathlib import Path
 import database
 
@@ -27,6 +28,11 @@ class DropArea(QFrame):
 
         self.setAcceptDrops(True)
 
+        self.image = None
+
+    def set_image(self, image): # image is QImage or None
+        self.image = image
+
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():   # if file(s) have been dragged
             event.acceptProposedAction() # Allow drop
@@ -37,7 +43,44 @@ class DropArea(QFrame):
         )
 
         if is_pdf(file_path):
+            # Keep the document
             self.parent().set_document(file_path)
+
+            # and set the thumbnail iamge
+            self.set_image(
+                self.render_pdf(file_path)
+            )
+
+    # Paint the PDF thumbnail
+    def paintEvent(self, event):
+
+        if self.image is not None:
+            painter = QtGui.QPainter(self)
+            painter.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing, True)
+
+            scaled_image = self.image.scaled(self.size(), QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+
+            # Calculating the center of the widget
+            x = (self.width() - scaled_image.width()) // 2
+            y = (self.height() - scaled_image.height()) // 2
+
+            # Draw the image at the center of the widget
+            painter.drawImage(QtCore.QPoint(x, y), scaled_image)
+        else:
+            super().paintEvent(event)
+
+    def render_pdf(self, file_path: Path): # Returns a QImage object
+
+        doc = QPdfDocument(self)
+
+        url = QtCore.QUrl.fromLocalFile(str(file_path))
+        doc.load(url.toLocalFile())
+
+        image = doc.render(0, self.size())
+        doc.deleteLater()
+
+        return image
+
 
 class Database_row_widget(QWidget):
     def __init__(self, parent = None):
@@ -87,6 +130,8 @@ class Database_row_widget(QWidget):
         self.title_edit.setText(None)
         self.authors_edit.setText(None)
         self.document_path = None
+        self.drop_area.set_image(None)
+        self.update() # Repaint the widget (especially the thumbnail.)
 
 class MainWindow(QMainWindow):
     def __init__(self):
